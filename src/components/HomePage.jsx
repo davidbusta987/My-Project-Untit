c49ba8c8e8905bcadf21a843c5ad6169e19e4240
davidbusta987 <52723075+davidbusta987@users.noreply.github.com>
2020-03-10 16:45:42 -0400

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,27 +7,7 @@ class HomePage extends Component {
 
         return (
             <div className="home-page">
-                <main>
-                   <section className="upper-section"> 
-                      <div className="rigth-columm">
-                          <h1 className="title">
-                                Improve student outcomes and insights. 
-                          </h1>
-                          <p className="description">
-                            Analize studennt brain capacities and gives the tools to thake students to the next level. 
-                          </p>
-                          <div className="links">
-                              <a className="demo">
-                                    SEE THE DEMO
-                              </a>
-                              <br/>
-                              <a className="create-account">
-                                    CREATE ACCOUNT
-                              </a>
-                          </div>
-                      </div>
-                   </section> 
-                </main>
+                
             </div>
         )
     }
