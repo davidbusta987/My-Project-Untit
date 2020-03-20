8f01e281810b81701168592aa59d3501596349e6
David Bustamante <52723075+davidbusta987@users.noreply.github.com>
2020-03-20 12:50:38 -0400

    Update Classes.jsx

--- src/layout/Classes.jsx
+++ src/layout/Classes.jsx
@@ -1,19 +1,33 @@
-import React ,{Component} from 'react'
-import Paper from '@material-ui/core/Paper';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import firebase from '../firebase/firebase';
 
 class Classes extends Component {
-    constructor() {
-        super();
-        this.state = {}
-    }
-    render() {
-        return (
-                  <Paper elevation={3}>
-                     <h4>My Class</h4>
-                 </Paper>
-         )
-    }
- 
-}
+  constructor(props) {
+    super(props);
+     this.state = {
+      lessons: []
+    };
+     let ref = firebase.firestore().collection("lessons").doc("SqYGqJRXFfMWE4CWKlpIwUmbeBQ2");
+     ref.onSnapshot(doc => {
+          this.setState({
+            lessons: doc.data()
+         })
+     })
+   }
+       
+     
 
-export default Classes 
+    render() {
+        return(
+            <div className="classes" >
+             <h1 className="card-1">{this.state.lessons.lesson1}</h1>
+             <h1 className="card-2" >{this.state.lessons.lessons2}</h1>
+             <h1 className="card-3" >{this.state.lessons.lessons3}</h1>
+            </div>
+        )
+        }
+    }
+
+export default Classes
